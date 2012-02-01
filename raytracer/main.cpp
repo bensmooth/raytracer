35c0b5beb89ced0e0c5a461b5b30028677113c2d
Benjamin Hart <hartj017@d.umn.edu>
2012-02-01 12:42:14 -0600

    Fixed build issues.
    Former-commit-id: e6e64d3412db7679477164024cdc4033092b2d9e

--- raytracer/main.cpp
+++ raytracer/main.cpp
@@ -1,155 +1,66 @@
-#include "Vector3D.h"
-#include "XMLSceneParser.h"
+#include <iostream>
+#include <cstdlib>
+
 #include "handleGraphicsArgs.h"
-#include "png++/png.hpp"
-#include "Random.h"
+#include "Scene.h"
+#include "RaytraceException.h"
 
+
+using namespace std;
 using namespace sivelab;
-
-class CameraCreator : public SceneElementCreator
-{
-public:
-  CameraCreator() {}
-  ~CameraCreator() {}
-
-  void instance(std::map<std::string, SceneDataContainer> &sdMap)
-  {
-    std::istringstream buf;
-    std::map<std::string, SceneDataContainer>::iterator sdIter;
-
-    std::string name, type;
-    Vector3D position, viewDir, lookatPoint;
-    bool lookatSet = false;
-    float focalLength;
-    float imagePlaneWidth;
-
-    sdIter = sdMap.find("camera_name");
-    assert( sdIter != sdMap.end() );
-    name = sdIter->second.val;
-
-    sdIter = sdMap.find("camera_type");
-    assert( sdIter != sdMap.end() );
-    type = sdIter->second.val;
-
-    sdIter = sdMap.find("camera_position");
-    assert( sdIter != sdMap.end() );
-    buf.str(sdIter->second.val);
-    buf >> position;
-    buf.clear();
-
-    sdIter = sdMap.find("camera_viewDir");
-    assert( sdIter != sdMap.end() );
-    buf.str(sdIter->second.val);
-    buf >> viewDir;
-    buf.clear();
-
-    sdIter = sdMap.find("camera_lookatPoint");
-    assert( sdIter != sdMap.end() );
-    lookatSet = sdIter->second.isSet;
-    if (lookatSet)
-      {
-	buf.str(sdIter->second.val);
-	buf >> lookatPoint;
-	buf.clear();
-      }
-
-    sdIter = sdMap.find("camera_focalLength");
-    assert( sdIter != sdMap.end() );
-    buf.str(sdIter->second.val);
-    buf >> focalLength;
-    buf.clear();
-
-    sdIter = sdMap.find("camera_imagePlaneWidth");
-    assert( sdIter != sdMap.end() );
-    buf.str(sdIter->second.val);
-    buf >> imagePlaneWidth;
-    buf.clear();
-
-    std::cout << "Camera: name=" << name << ", type=" << type << std::endl;
-    std::cout << "\tposition: " << position << std::endl;
-    if (lookatSet)
-      std::cout << "\tlook at point: " << lookatPoint << std::endl;
-    else 
-      std::cout << "\tview direction: " << viewDir << std::endl;
-    std::cout << "\tfocal length: " << focalLength << std::endl;
-    std::cout << "\timage plane width: " << imagePlaneWidth << std::endl;
-  }
-};
-
-class GenericCreator : public SceneElementCreator
-{
-public:
-  GenericCreator() {}
-  ~GenericCreator() {}
-
-  void instance(std::map<std::string, SceneDataContainer> &sdMap)
-  {
-    if (m_otype == CAMERA)
-      std::cout << "SceneElementCreator type =  CAMERA" << std::endl;
-    else if (m_otype == LIGHT)
-      std::cout << "SceneElementCreator type =  LIGHT" << std::endl;
-    else if (m_otype == SHADER)
-      std::cout << "SceneElementCreator type =  SHADER" << std::endl;
-    else if (m_otype == SHAPE)
-      std::cout << "SceneElementCreator type =  SHAPE" << std::endl;
-    else 
-      std::cout << "BOGUS TYPE" << std::endl;
-    
-
-    // Figure out how to instance the other things...
-    std::map<std::string, SceneDataContainer>::const_iterator sdIter;
-    for (sdIter=sdMap.begin(); sdIter != sdMap.end(); sdIter++)
-      {
-	SceneDataContainer sd = sdIter->second;
-	std::cout << "Data [" << sdIter->first << "]: " 
-		  << sd.dtype << ", " << sd.name << ", " 
-		  << sd.val << ", " << sd.isSet << std::endl;
-      }
-  }
-};
-
-class ShapeCreator : public SceneElementCreator
-{
-public:
-  ShapeCreator() {}
-  ~ShapeCreator() {}
-
-  void instance(std::map<std::string, SceneDataContainer> &sdMap)
-  {
-    // Figure out how to instance the other things...
-    std::map<std::string, SceneDataContainer>::const_iterator sdIter;
-    for (sdIter=sdMap.begin(); sdIter != sdMap.end(); sdIter++)
-      {
-	SceneDataContainer sd = sdIter->second;
-	std::cout << "Shape [" << sdIter->first << "]: " 
-		  << sd.dtype << ", " << sd.name << ", " 
-		  << sd.val << ", " << sd.isSet << std::endl;
-      }
-  }
-};
 
 
 int main(int argc, char *argv[])
 {
-  GraphicsArgs args;
-  args.process(argc, argv);
+	GraphicsArgs args;
+	args.process(argc, argv);
 
-  XMLSceneParser xmlScene;
+	// This is the scene object that we primarily interact with.
+	Scene *scene = NULL;
 
-  // register object creation function with xmlScene
-  xmlScene.registerCallback("camera", new CameraCreator);
-  xmlScene.registerCallback("light", new GenericCreator);
-  xmlScene.registerCallback("shader", new GenericCreator);
-  xmlScene.registerCallback("shape", new ShapeCreator);
+	// Make sure they passed in an input filename.
+	if (args.inputFileName == "")
+	{
+		cerr << "Need input file!" << endl;
+		exit(EXIT_FAILURE);
+	}
 
-  if (args.inputFileName != "")
-    xmlScene.parseFile( args.inputFileName );
-  else
-    {
-      std::cerr << "Need input file!" << std::endl;
-      exit(EXIT_FAILURE);
-    }
+	// Try to read in the given scene file.
+	try
+	{
+		scene = new Scene(args.inputFileName);
+	}
+	catch (RaytraceException &e)
+	{
+		cerr << "Error reading in scene file " << args.inputFileName << ": " << e.what() << endl;
+		exit(EXIT_FAILURE);
+	}
 
-  exit(EXIT_SUCCESS);
+	// Make sure they passed in the output filename.
+	if (args.outputFileName == "")
+	{
+		cerr << "Need output file!" << endl;
+		exit(EXIT_FAILURE);
+	}
+
+	// Make sure they passed in a width and a height.
+	if ((args.width <= 0) || (args.height <= 0))
+	{
+		cerr << "You must pass in a width and a height!" << endl;
+		exit(EXIT_FAILURE);
+	}
+
+	// Try to render the scene.
+	try
+	{
+		scene->Render(args.outputFileName, args.width, args.height);
+	}
+	catch (RaytraceException &e)
+	{
+		cerr << "Error rendering scene: " << e.what() << endl;
+		exit(EXIT_FAILURE);
+	}
+
+	exit(EXIT_SUCCESS);
 }
 
--- raytracerLib/Scene.cpp
+++ raytracerLib/Scene.cpp
@@ -3,12 +3,161 @@
 #include <cfloat>
 #include <png++/image.hpp>
 
+#include "png++/png.hpp"
+#include "Vector3D.h"
+#include "XMLSceneParser.h"
 #include "Intersection.h"
+
+
+using namespace sivelab;
+
+/**
+ * This creator is used to create the camera.
+ */
+class CameraCreator : public SceneElementCreator
+{
+public:
+  CameraCreator() {}
+  ~CameraCreator() {}
+
+  void instance(std::map<std::string, SceneDataContainer> &sdMap)
+  {
+    std::istringstream buf;
+    std::map<std::string, SceneDataContainer>::iterator sdIter;
+
+    std::string name, type;
+    Vector3D position, viewDir, lookatPoint;
+    bool lookatSet = false;
+    float focalLength;
+    float imagePlaneWidth;
+
+    sdIter = sdMap.find("camera_name");
+    assert( sdIter != sdMap.end() );
+    name = sdIter->second.val;
+
+    sdIter = sdMap.find("camera_type");
+    assert( sdIter != sdMap.end() );
+    type = sdIter->second.val;
+
+    sdIter = sdMap.find("camera_position");
+    assert( sdIter != sdMap.end() );
+    buf.str(sdIter->second.val);
+    buf >> position;
+    buf.clear();
+
+    sdIter = sdMap.find("camera_viewDir");
+    assert( sdIter != sdMap.end() );
+    buf.str(sdIter->second.val);
+    buf >> viewDir;
+    buf.clear();
+
+    sdIter = sdMap.find("camera_lookatPoint");
+    assert( sdIter != sdMap.end() );
+    lookatSet = sdIter->second.isSet;
+    if (lookatSet)
+      {
+	buf.str(sdIter->second.val);
+	buf >> lookatPoint;
+	buf.clear();
+      }
+
+    sdIter = sdMap.find("camera_focalLength");
+    assert( sdIter != sdMap.end() );
+    buf.str(sdIter->second.val);
+    buf >> focalLength;
+    buf.clear();
+
+    sdIter = sdMap.find("camera_imagePlaneWidth");
+    assert( sdIter != sdMap.end() );
+    buf.str(sdIter->second.val);
+    buf >> imagePlaneWidth;
+    buf.clear();
+
+    std::cout << "Camera: name=" << name << ", type=" << type << std::endl;
+    std::cout << "\tposition: " << position << std::endl;
+    if (lookatSet)
+      std::cout << "\tlook at point: " << lookatPoint << std::endl;
+    else 
+      std::cout << "\tview direction: " << viewDir << std::endl;
+    std::cout << "\tfocal length: " << focalLength << std::endl;
+    std::cout << "\timage plane width: " << imagePlaneWidth << std::endl;
+  }
+};
+
+
+/**
+ * This creator is used for elements that we don't know how to create yet.
+ */
+class GenericCreator : public SceneElementCreator
+{
+public:
+  GenericCreator() {}
+  ~GenericCreator() {}
+
+  void instance(std::map<std::string, SceneDataContainer> &sdMap)
+  {
+    if (m_otype == CAMERA)
+      std::cout << "SceneElementCreator type =  CAMERA" << std::endl;
+    else if (m_otype == LIGHT)
+      std::cout << "SceneElementCreator type =  LIGHT" << std::endl;
+    else if (m_otype == SHADER)
+      std::cout << "SceneElementCreator type =  SHADER" << std::endl;
+    else if (m_otype == SHAPE)
+      std::cout << "SceneElementCreator type =  SHAPE" << std::endl;
+    else 
+      std::cout << "BOGUS TYPE" << std::endl;
+    
+
+    // Figure out how to instance the other things...
+    std::map<std::string, SceneDataContainer>::const_iterator sdIter;
+    for (sdIter=sdMap.begin(); sdIter != sdMap.end(); sdIter++)
+      {
+	SceneDataContainer sd = sdIter->second;
+	std::cout << "Data [" << sdIter->first << "]: " 
+		  << sd.dtype << ", " << sd.name << ", " 
+		  << sd.val << ", " << sd.isSet << std::endl;
+      }
+  }
+};
+
+
+/**
+ * This class is used to create scene objects.
+ */
+class ObjectCreator : public SceneElementCreator
+{
+public:
+  ObjectCreator() {}
+  ~ObjectCreator() {}
+
+  void instance(std::map<std::string, SceneDataContainer> &sdMap)
+  {
+    // Figure out how to instance the other things...
+    std::map<std::string, SceneDataContainer>::const_iterator sdIter;
+    for (sdIter=sdMap.begin(); sdIter != sdMap.end(); sdIter++)
+      {
+	SceneDataContainer sd = sdIter->second;
+	std::cout << "Shape [" << sdIter->first << "]: " 
+		  << sd.dtype << ", " << sd.name << ", " 
+		  << sd.val << ", " << sd.isSet << std::endl;
+      }
+  }
+};
 
 
 Scene::Scene(std::string filename)
 {
 	// Parse the XML scene file.
+	XMLSceneParser xmlScene;
+
+	// Register object creation handlers with the scene parser.
+	// TODO: Are these all memory leaks?
+	xmlScene.registerCallback("camera", new CameraCreator());
+	xmlScene.registerCallback("light", new GenericCreator());
+	xmlScene.registerCallback("shader", new GenericCreator());
+	xmlScene.registerCallback("shape", new ObjectCreator());
+
+	xmlScene.parseFile(filename);
 }
 
 
@@ -22,8 +171,9 @@ void Scene::Render(std::string outfile, int imageWidth, int imageHeight)
 	{
 		for (int imageY = 0; imageY < imageHeight; imageY++)
 		{
-			// Calculate ray we need to shoot for this pixel.
-			Ray ray = m_camera->CalculateViewingRay(imageX, imageY);
+			// TODO: Calculate ray we need to shoot for this pixel.
+			//Ray ray = m_camera->CalculateViewingRay(imageX, imageY);
+			Ray ray;
 
 			// See if ray intersects any objects.
 			Intersection closestIntersect;
@@ -51,9 +201,9 @@ void Scene::Render(std::string outfile, int imageWidth, int imageHeight)
 			}
 			else
 			{
-				// Use black as the background color for now.
+				// Use green as the background color for now.
 				color.red = 0;
-				color.green = 0;
+				color.green = 255;
 				color.blue = 0;
 			}
 

19ed981a742348ae75535555aeb0ee9321585b3f
Ben Hart <hartj017@d.umn.edu>
2012-02-06 23:43:04 -0600

    Implemented constructor.
    Former-commit-id: a5fb51813c8e2f2a1904c5ea9f57cb5b06d135ed

--- raytracerLib/SolidShader.cpp
+++ raytracerLib/SolidShader.cpp
@@ -1,6 +1,12 @@
 #include "SolidShader.h"
 #include "Color.h"
 
+SolidShader::SolidShader(const Color& color)
+{
+	m_color = color;
+}
+
+
 Color SolidShader::Shade(Intersection& intersection)
 {
 	return (m_color);

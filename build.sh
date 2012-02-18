9e988670355e301bff121805e656907fd467b815
Ben Hart <hartj017@d.umn.edu>
2012-02-17 23:45:13 -0600

    Added a script that builds the code and then does a bunch of renders.
    Former-commit-id: be25eef143426f0d6b74ecbf200b01f85707589f

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# Make some folders.
+mkdir build
+mkdir renders
+cd build
+
+# Build it.
+cmake .. -DCMAKE_BUILD_TYPE=Release
+cd raytracer
+make -j
+
+# Render each scene.
+cd ../../SceneFiles/
+for i in *.xml
+do
+    if test -f "$i"
+    then
+       echo "Rendering $i.."
+       ../build/raytracer/raytracer -i $i -o ../renders/$i.png -w 600 -h 600
+    fi
+done
+
+echo "Done rendering!"

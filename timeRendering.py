59686c80308f786d3aaf1192a3551200b4f85618
Ben Hart <bensmooth@gmail.com>
2014-06-04 00:40:37 -0500

    Added sample count.
    Former-commit-id: b97ea00516f7055cd3231b4a3c1e118fbe3d7dc4

--- timeRendering.py
+++ timeRendering.py
@@ -1,20 +1,25 @@
 #!/usr/bin/python
 import time
 import os
+import sys
 from glob import glob
 from subprocess import call
 
 scenePath = './SceneFiles/'
 raytracerPath = './build/raytracer/raytracer'
-raytracerArguments = '{0} -i ./SceneFiles/{1} -o ./renders/{1}.png -w 100 -h 100 -r 9'
+raytracerArguments = '{0} -i ./SceneFiles/{1} -o ./renders/{1}.png -w 100 -h 100 -r 4'
 
+# Get number of times to benchmark.
+if len(sys.argv) == 2:
+	sampleCount = int(sys.argv[1])
+else:
+	sampleCount = 1
+
+# Create list of scene files to render.
 sceneFiles = [ x.replace(scenePath, '') for x in glob(scenePath + "*.xml") ]
 sceneFiles.sort()
 
-print('Command line used: ' + raytracerArguments)
-
-# Print out heading of scene names.
-print('Scene', end=',')
+# Print out scene names.
 for filename in sceneFiles:
 	print (filename, end=',')
 
@@ -22,9 +27,9 @@ print()
 
 # Run the raytracer with each configuration.
 with open(os.devnull, "w") as fnull:
-	for scene in sceneFiles:
-		start_time = time.time()
-		call(raytracerArguments.format(raytracerPath, scene).split(), stdout = fnull, stderr = fnull)
-		print(time.time() - start_time, end=',')
-
-print()
+	for i in range(sampleCount):
+		for scene in sceneFiles:
+			start_time = time.time()
+			call(raytracerArguments.format(raytracerPath, scene).split(), stdout = fnull, stderr = fnull)
+			print(time.time() - start_time, end=',')
+		print()
